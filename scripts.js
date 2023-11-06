ffda90456cb410f26055b6217f0180f5e59de703
Noan Jacquelin <noanjacquelin@yahoo.com>
2023-11-06 17:19:39 +0100

    Commit

--- scripts.js
+++ scripts.js
@@ -8,3 +8,27 @@ function redirectionSelect() {
     window.location.href = "lesprojets.html";
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  let dropbtn = document.querySelector(".dropbtn");
+  let dropdownContent = document.querySelector(".dropdown-content");
+
+  dropbtn.addEventListener("mouseover", function () {
+    dropdownContent.classList.add("show");
+  });
+
+  dropbtn.addEventListener("mouseout", function () {
+    dropdownContent.classList.remove("show");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  let dropdownOptions = document.querySelectorAll(".dropdown-content a");
+
+  dropdownOptions.forEach(function (option) {
+    option.addEventListener("click", function () {
+      let pageURL = option.getAttribute("href");
+      window.location.href = pageURL;
+    });
+  });
+});
