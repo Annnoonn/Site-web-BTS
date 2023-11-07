9077a154902e45fb99b9bb832e9a68177770f07a
Noan Jacquelin <noanjacquelin@yahoo.com>
2023-11-07 11:25:47 +0100

    Last version

--- scripts.js
+++ scripts.js
@@ -1,34 +1,51 @@
-function redirectionSelect() {
-  let idMenuderoulant = document.getElementById("menuDeroulant");
-  if (idMenuderoulant.value === "La Promo") {
-    window.location.href = "lapromo.html";
-  } else if (idMenuderoulant.value === "Les Intervenants") {
-    window.location.href = "lesintervenants.html";
-  } else if (idMenuderoulant.value === "Les Projets") {
-    window.location.href = "lesprojets.html";
-  }
+function displayGallery() {
+  const photos = [
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+    "images/placeholder.png",
+  ];
+
+  photos.forEach((photo) => {
+    const img = document.createElement("img");
+    img.src = photo;
+    imgGrid.appendChild(img);
+  });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  let dropbtn = document.querySelector(".dropbtn");
-  let dropdownContent = document.querySelector(".dropdown-content");
+function main() {
+  let menuBtn = document.querySelector(".menuDeroulantBtn");
+  let listOptions = document.querySelector(".listOptions");
+  console.log(listOptions);
 
-  dropbtn.addEventListener("mouseover", function () {
-    dropdownContent.classList.add("show");
+  menuBtn.addEventListener("mouseenter", function () {
+    listOptions.classList.toggle("show");
   });
 
-  dropbtn.addEventListener("mouseout", function () {
-    dropdownContent.classList.remove("show");
+  menuBtn.addEventListener("mouseleave", function () {
+    listOptions.classList.remove("show");
   });
-});
 
-document.addEventListener("DOMContentLoaded", function () {
-  let dropdownOptions = document.querySelectorAll(".dropdown-content a");
-
-  dropdownOptions.forEach(function (option) {
-    option.addEventListener("click", function () {
-      let pageURL = option.getAttribute("href");
-      window.location.href = pageURL;
-    });
+  listOptions.addEventListener("mouseenter", function () {
+    listOptions.classList.toggle("show");
   });
-});
+  listOptions.addEventListener("mouseleave", function () {
+    listOptions.classList.remove("show");
+  });
+
+  displayGallery();
+}
